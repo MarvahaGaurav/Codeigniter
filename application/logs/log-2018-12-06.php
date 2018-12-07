787d5747e7020cb2f4244dddbd48bde271416656
Parmar93 <parmar.singh@appinventiv.com>
2018-12-07 11:23:39 +0530

    logs file

---
+++ application/logs/log-2018-12-06.php
@@ -0,0 +1,75 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-06 04:46:05 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-06 04:46:05 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:46:06 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:46:06 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:46:07 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:46:07 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:46:12 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:46:12 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:47:19 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:47:19 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:47:29 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:47:29 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:47:29 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:47:29 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:15 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:15 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:17 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:17 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:18 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:18 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:19 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:19 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:19 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:19 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:42 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:42 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:48:42 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:48:42 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:49:12 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:49:12 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:49:46 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:49:46 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:50:06 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:06 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:50:09 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:09 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:50:23 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:35 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:35 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:50:48 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:48 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:50:57 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:50:57 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:51:03 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:51:03 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:51:54 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:51:54 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 04:51:59 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 04:51:59 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:03:53 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:03:53 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:03:53 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:03:53 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:04:10 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:04:10 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:04:21 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:04:21 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:05:30 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:05:31 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:06:08 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:06:08 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:07:38 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:07:39 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:09:32 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:09:32 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:09:43 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:09:43 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:16:04 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:16:04 --> 404 Page Not Found: /index
+ERROR - 2018-12-06 05:26:03 --> Could not find the specified $config['composer_autoload'] path: application/composer/vendor/autoload.php
+ERROR - 2018-12-06 05:26:03 --> 404 Page Not Found: /index
