6d8d19710f63f1bd82caa0a25256919ba7fb5c49
Appinventiv-pc <nirmalsingh.chauhan@appinventiv.com>
2018-12-17 10:21:07 +0530

    quotes section for user

---
+++ application/views/mail/sendquote.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+  <title>Send Quote</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <table cellpadding="0" cellspacing="0" border="0" style="margin:0 auto; min-width:320px; max-width:600px; border:2px solid #d60014;" width="100%">
+  <tr style="  background:#cecece; padding:10px;">
+  <td align="center" style="padding: 10px;"><a href="http://sg-as.com"><img width="180" height="auto" src="http://smartguide-dev.applaurels.com/public/images/logo.png" alt="Send Quote"></a></td>
+  </tr>
+  
+  <tr><td>&nbsp; </td> </tr>
+  <tr><td>&nbsp; </td> </tr>
+  <tr><td>&nbsp; </td> </tr>
+  
+  <tr>
+  <td style="   color: #383636;
+    font-family: arial;
+    font-size: 20px;
+    padding: 0 35px;">Hi <?php echo $name ?> </td>
+  </tr>
+  
+    <tr>
+        <td style="color: #383636;
+          font-family: arial;
+          font-size: 16px;
+          line-height: 26px;
+          padding: 35px">Your quote is submitted by installer,<br>
+          Thank you Choosing us.
+        </td>
+  </tr>
+  
+  
+    <tr>
+  <td style="color: #383636;
+    font-family: arial;
+    font-size: 16px;
+    line-height: 26px;
+    padding: 35px">
+    <strong>Warm Regards,<br> 
+    The Smart Guide Team,</strong>
+  </td>
+  </tr>  
+  
+  
+  </table>
