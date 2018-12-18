d2c0cba1a377242291a0ab7391d232191cf28a76
Amritanshu <rana.amritanshu@appinventiv.com>
2018-12-18 17:37:43 +0530

    notification redirections added

--- application/modules/web/controllers/NotificationController.php
+++ application/modules/web/controllers/NotificationController.php
@@ -22,6 +22,8 @@ class NotificationController extends BaseController
 
             $this->requestData = $this->input->get();
             $this->load->helper(['datetime']);
+            $this->load->config('css_config');
+            $this->data['css'] = $this->config->item('basic-with-font-awesome');
 
             $page = isset($this->requestData['page'])?abs((int)$this->requestData['page']):1;
 
@@ -44,10 +46,11 @@ class NotificationController extends BaseController
                     );
                     unset($notification['messages']);
                 }
+                $notification['redirection'] = $this->redirectionHandler()[$notification['type']];
+                $notification['redirection'] = !empty($notification['redirection'])?base_url($notification['redirection']):null;
                 return $notification;
             }, $notifications);
             
-
             $this->data['notifications'] = $notifications;
             $this->load->library('Commonfn');
             $this->data['links'] = $this->commonfn->pagination(uri_string(), $notificationCount, $params['limit']);
@@ -56,5 +59,18 @@ class NotificationController extends BaseController
         } catch (\Exception $error) {
             show404($this->lang->line('internal_server_error'), base_url('home/applications'));
         }
+    }
+
+    private function redirectionHandler()
+    {
+        return [
+            NOTIFICATION_EMPLOYEE_REQUEST_RECEIVED => "home/technicians/requests",
+            NOTIFICATION_RECEIVED_QUOTES => "home/quotes/awaiting",
+            NOTIFICATION_PERMISSION_GRANTED => null,
+            NOTIFICATION_SEND_QUOTES => null,
+            NOTIFICATION_ACCEPT_QUOTE => "home/quotes/approved",
+            NOTIFICATION_EDIT_QUOTE_PRICE => null,
+            NOTIFICATION_EMPLOYEE_APPROVED => null
+        ];
     }
 }
--- application/modules/web/views/notifications/notification.php
+++ application/modules/web/views/notifications/notification.php
@@ -16,13 +16,17 @@
     
         <?php foreach ($notifications as $notification) { ?>
             <div class="well well-lg">
-                <div>
-                    <span><img src="<?php echo empty($notification['sender']['image'])?base_url('public/images/user-placeholder.png'):$notification['sender']['image'] ?>" alt="<?php $notification['id'] ?>" height="200px" width="200"></span>
-                    <span><?php echo $notification['message'] ?></span>
-                </div>
-                <div class="clearfix col-md-12">
-                    <div class="pull-right col-md-3"><?php echo convert_date_time_format("Y-m-d H:i:s", $notification['created_at'], 'g:i A, M d Y') ?></div>
-                </div>
+                <a href="<?php echo !empty($notification['redirection'])?$notification['redirection']:'javascript:void(0)' ?>">
+                    <div>
+                        
+                        <span><img src="<?php echo empty($notification['sender']['image'])?base_url('public/images/user-placeholder.png'):$notification['sender']['image'] ?>" alt="<?php $notification['id'] ?>" height="200px" width="200"></span>
+                        <span><?php echo $notification['message'] ?></span>
+                        
+                    </div>
+                    <div class="clearfix col-md-12">
+                        <div class="pull-right col-md-3"><?php echo convert_date_time_format("Y-m-d H:i:s", $notification['created_at'], 'g:i A, M d Y') ?></div>
+                    </div>
+                </a>
             </div>
         <?php }?>
 
