5410d611773fd6d67dca9fe4f00d835f1feedca8
Amritanshu <rana.amritanshu@appinventiv.com>
2018-12-20 01:47:22 +0530

    search product changes

--- public/js/main/select_product.js
+++ public/js/main/select_product.js
@@ -6,20 +6,21 @@ requirejs.config( {
         bootstrap: "bootstrap.min",
         common: "web/common",
         jqueryScrollbar: "plugin/jquery.scrollbar.min",
-        helper: 'web/helpers/select_product'
-
+        helper: 'web/helpers/select_product',
+        searchProduct: "web/helpers/select_product_search"
     },
     shim: {
         //dependencies
         bootstrap: [ 'jquery' ],
         common: [ 'bootstrap' ],
         jqueryScrollbar: [ 'jquery' ],
-        helper: [ 'jquery' ]
+        helper: [ 'jquery' ],
+        searchProduct: ['helper']
     }
 } );
 
 requirejs(
-        [ "jquery", "bootstrap", "common", "jqueryScrollbar", "helper" ],
+        [ "jquery", "bootstrap", "common", "jqueryScrollbar", "helper", 'searchProduct' ],
         function ( $ ) {
 
         },
